16925a3b19225b4b661cdcd7b2cd99c19b049351
mahmoud elhamrawi <mahmoudelhamrawi2020@gmail.com>
2019-10-07 00:34:22 +0200

    frist change

---
+++ js/mine.js
@@ -0,0 +1,25 @@
+
+
+$("#options i").click(function(){
+    
+    
+    $("#color").toggle();
+});
+let lis = $("#options ul li");
+
+
+
+lis.eq(0).css("backgroundcolor","#09c")
+lis.eq(1).css("backgroundcolor","green")
+lis.eq(2).css("backgroundcolor","gray")
+lis.eq(3).css("backgroundcolor","white")
+lis.eq(4).css("backgroundcolor","black")
+
+
+
+ 
+
+
+
+
+ 
